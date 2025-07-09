861168656d1fad2850dab04513368bbfdd780de0
Kefas Kingsley <kefasiceking@gmail.com>
2025-07-09 20:03:32 +0100

    feat: create database schema for tables

---
+++ migrations/0003_create_messages_table.sql
@@ -0,0 +1,11 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS messages (
+    id UUID PRIMARY KEY,
+    room_id UUID NOT NULL REFERENCES rooms(id) ON DELETE CASCADE,
+    sender_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS idx_messages_room_id ON messages(room_id);
+CREATE INDEX IF NOT EXISTS idx_messages_sender_id ON messages(sender_id);
